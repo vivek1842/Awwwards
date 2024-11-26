f00dd48b4484dc0e4d87aba3b2c4397c27393e23
vivek1842 <vivekghule2002@gmail.com>
2024-11-26 23:31:31 +0530

    assests added

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <main>
+      <h1 className='text-5xl text-orange-500 font-bold'>Welcome to Awwwards</h1>
+    </main>
   )
 }
 
