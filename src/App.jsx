9c6c467cceb914c306574a54fd0539740e2649a5
vivek1842 <vivekghule2002@gmail.com>
2024-11-29 23:57:26 +0530

    colours added to tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const App = () => {
   return (
     <main>
-      <h1 className='text-5xl text-orange-500 font-bold font-circular-web'>Welcome to Awwwards</h1>
+      <h1 className='text-5xl text-orange-500 font-bold'>Welcome to Awwwards</h1>
     </main>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,19 @@ export default {
         'circular-web': ['circular-web', 'sans-serif'],
         'robert-medium': ['robert-medium', 'sans-serif'],
         'robert-regular': ['robert-regular', 'sans-serif'],
+      },
+      colors: {
+        blue: {
+          50: '#dfdff0',
+          75: '#dfdff2',
+          100: '#f0f2fa',
+          200: '#010101',
+          300: '#4fb7dd'
+        },
+        violet: {
+          300: '#5724ff'
+        },
+        
       }
     },
   },
