6fe37fc55cbd991d36570246c3b357cda7655cad
vivek1842 <vivekghule2002@gmail.com>
2025-01-03 00:41:29 +0530

    hero/button added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,7 @@
 /* eslint-disable no-unused-vars */
 import { useRef, useState } from "react"
+import Button from "./Button";
+import { TiLocationArrow } from "react-icons/ti";
 
 
 const Hero = () => {
@@ -71,10 +73,29 @@ const Hero = () => {
           />
         </div>
 
-        <h1 className="special-font hero-heading absolute bottom-5 right-5 z-40 text-blue-75">
+        <h1 className='special-font hero-heading absolute bottom-5 right-5 z-40 text-blue-75'>
             G<b>a</b>ming
         </h1>
+
+        <div className='absolute top-0 left-0 z-40 size-full'>
+          <div className='mt-24 px-5 sm:px-10'>
+            <h1 className='special-font hero-heading text-blue-100'>
+              Redifi<b>n</b>e
+            </h1>
+
+            <p className='mb-5 max-w-64 font-robert-regular text-blue-100'>
+              Enter the Metagame Layer <br /> Unleash the Play Economy
+            </p>
+
+            <Button id='watch-trailer' title='Watch Trailer' leftIcon={<TiLocationArrow />} containerClass='!bg-yellow-300 flex-center gap-1'/>
+          </div>
+        </div>
       </div>
+
+      <h1 className='special-font hero-heading absolute bottom-5 right-5 z-40 text-blue-75'>
+        G<b>a</b>ming
+      </h1>
+
     </div>
   )
 }
