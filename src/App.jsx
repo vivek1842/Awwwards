c9dcc815d3c474e265433caaaff6b7dd77ecbcaf
vivek1842 <vivekghule2002@gmail.com>
2025-01-04 23:36:46 +0530

    hero section completed

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ const App = () => {
   return (
     <main className='relative min-h-screen w-screen overflow-x-hidden'>
      <Hero />
+     <section className='z-0 min-h-screen bg-blue-500' />
     </main>
   )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,17 @@
 /* eslint-disable no-unused-vars */
-import { useRef, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import Button from "./Button";
 import { TiLocationArrow } from "react-icons/ti";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
 
+gsap.registerPlugin(ScrollTrigger);
 
 const Hero = () => {
   const [currentIndex, setCurrentIndex] = useState(1);
   const [hasClicked, setHasClicked] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
   const [loadedVidoes, setLoadedVidoes] = useState(0);
 
   const totalVideos = 4;
@@ -34,6 +36,12 @@ const Hero = () => {
     console.log(upcomingVideoIndex);
     console.log(currentIndex);
   }
+
+  useEffect(()=> {
+    if(loadedVidoes === totalVideos -1) {
+      setIsLoading(false);
+    }
+  }, [loadedVidoes])
 
   // animation of miniplayer
   useGSAP(()=>{
@@ -83,6 +91,17 @@ const Hero = () => {
 
   return (
     <div className='relative h-dvh w-screen overflow-x-hidden'>
+
+      {isLoading && (
+        <div className="flex-center absolute z-[100] h-dvh w-screen overflow-hidden bg-violet-50">
+          <div className="three-body">
+            <div className="three-body__dot" />
+            <div className="three-body__dot" />
+            <div className="three-body__dot" />
+          </div>
+        </div>
+      )}
+
       <div id='video-frame' className='relative h-dvh z-10 w-screen overflow-hidden rounded-lg bg-blue-75'>
         <div>
           <div className='mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg'>
