86e6d0db1dcf77924dfb8218ca99f2870d1bae18
vivek1842 <vivekghule2002@gmail.com>
2025-01-04 17:46:51 +0530

    gsap miniplay animation created

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,6 +2,8 @@
 import { useRef, useState } from "react"
 import Button from "./Button";
 import { TiLocationArrow } from "react-icons/ti";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 
 const Hero = () => {
@@ -32,6 +34,29 @@ const Hero = () => {
     console.log(upcomingVideoIndex);
     console.log(currentIndex);
   }
+
+  useGSAP(()=>{
+    if(hasClicked){
+      gsap.set('#next-video', {visibility: 'visible'});
+
+      gsap.to('#next-video', {
+        transformOrigin: 'center center',
+        scale : 1,
+        width: '100%',
+        height: '100%',
+        duration: 1,
+        ease: 'power1.inOut',
+        onStart: nextVideoRef.current.play(),
+      });
+
+      gsap.from('#current-video', {
+        transformOrigin: 'center center',
+        scale: 0,
+        duration: 1.5,
+        ease: 'power1.inOut'
+      })
+    }
+  },{dependencies: [currentIndex], revertOnUpdate: true})
 
   const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
 
