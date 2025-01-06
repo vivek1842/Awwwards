2608f745d360ab52a4b1729090f74f5dc3fa5da6
vivek1842 <vivekghule2002@gmail.com>
2025-01-07 00:00:19 +0530

    features section started

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import Hero from './components/Hero'
 import About from './components/About'
 import Navbar from './components/Navbar'
+import Features from './components/Features'
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
      <Navbar />
      <Hero />
      <About />
+     <Features />
     </main>
   )
 }
