6faa65d73e3c068fe302bb277625844fbbcdac3c
vivek1842 <vivekghule2002@gmail.com>
2025-01-08 00:24:39 +0530

    minor bug fixed

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,3 +1,4 @@
+import AnimatedTitle from "./AnimatedTitle"
 import Button from "./Button"
 
 /* eslint-disable react/prop-types */
@@ -14,7 +15,7 @@ const ImageClipBox = ({ src, clipClass }) =>{
 const Contact = () => {
   return (
     <div id="contact" className="my-20 min-h-96 w-screen px-10">
-        <div className="relative rounded-lg bg-black py-24 text-blue-50 md:overflow-hidden">
+        <div className="relative rounded-lg bg-black py-24 text-blue-50 sm:overflow-hidden">
             <div className="absolute -left-20 top-0 hidden h-full w-72 overflow-hidden sm:block lg:left-20 lg:w-96">
                 <ImageClipBox 
                     src="img/contact-1.webp"
@@ -42,9 +43,10 @@ const Contact = () => {
                     Joing Zentry
                 </p>
 
-                <p className="special-font mt-10 w-full font-zentry text-5xl leading-[0.9] md:text-[6rem]">
-                    Let&#39;s b<b>u</b>ild the <br /> new era of <br /> g<b>a</b>ming t<b>o</b>gether
-                </p>
+                <AnimatedTitle 
+                    title="Let&#39;s b<b>u</b>ild the <br /> new era of <br /> g<b>a</b>ming t<b>o</b>gether"
+                    className="special-font mt-10 w-full font-zentry text-5xl leading-[0.9] md:text-[6rem]"
+                />
 
                 <Button 
                     title="contact us"
