5ea6621b71a9503c0329055591b05a772eef6840
vivek1842 <vivekghule2002@gmail.com>
2025-01-08 00:15:33 +0530

    footer completed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,35 @@
+import { FaDiscord, FaGithub, FaTwitch, FaTwitter } from "react-icons/fa"
+
+const links = [
+    {href: 'https://discord.com', icon: <FaDiscord />},
+    {href: 'https://twitter.com', icon: <FaTwitter />},
+    {href: 'https://github.com', icon: <FaGithub />},
+    {href: 'https://twitch.com', icon: <FaTwitch />}
+
+
+]
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="w-screen bg-violet-300 py-4 text-black">
+        <div className="container mx-auto flex flex-col items-center justify-between gap-4 px-4 md:flex-row">
+            <p className="text-center text-sm font-light md:text-left">
+                &copy; Nova 2024. All rights reseved
+            </p>
+
+            <div className="flex justify-center gap-4 md:justify-start">
+                {links.map((link) => (
+                    <a key={link} href={link.href} target="_blank" rel="noopener noreferrer" className="text-black transition-colors duration-500 ease-in-out hover:text-white">
+                        {link.icon}
+                    </a>
+                ))}
+            </div>
+
+            <a href="#privacy-policy" className="text-center text-sm hover:underline md:text-right">
+                Privacy Policy
+            </a>
+        </div>
+    </footer>
   )
 }
 
