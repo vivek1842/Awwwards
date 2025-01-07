9c8a2f85baf45993c2d4f080e924ffa9d4c1a281
vivek1842 <vivekghule2002@gmail.com>
2025-01-07 17:21:05 +0530

    story started

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from './components/Hero'
 import About from './components/About'
 import Navbar from './components/Navbar'
 import Features from './components/Features'
+import Story from './components/Story'
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
      <Hero />
      <About />
      <Features />
+     <Story />
     </main>
   )
 }
